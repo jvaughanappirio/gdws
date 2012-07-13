87d99c10ff81b01940e9a35796213e0701cf9bf5
Jason Vaughan <jvaughan@appirio.com>
2012-07-12 23:53:15 -0700

    dependency removed

--- src/main/java/org/greendot/heroku/service/QMaster.java
+++ src/main/java/org/greendot/heroku/service/QMaster.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 
-import org.joda.time.*;
-import org.joda.time.format.*;
-
 @XmlRootElement(name="QMaster")
 public class QMaster
 {	
